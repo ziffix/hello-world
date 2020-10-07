b2827d669579de05bc985a89e7b1e691a9d25bb0
ziffix <55535764+ziffix@users.noreply.github.com>
2020-10-07 08:20:04 +0200

    Add files via upload

---
+++ Övning 1.6/src/main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+	
+public class main {
+
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Olle Gardell");
+		JOptionPane.showMessageDialog(null, "0739362667");
+		
+		
+
+	}
+
+}
